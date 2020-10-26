90b63a1e4a18e15b3091115fc173da2a42fbcbb3
Eddy Rivera <eddyrivera@ITs-MacBook-Pro.local>
2020-10-25 23:46:47 -0300

    add deploy variables

---
+++ README.md
@@ -0,0 +1,38 @@
+This project was bootstrapped with
+[Create React App](https://github.com/facebook/create-react-app).
+
+# Todo App
+
+This proyect uses node on the backend and react on the frontend.
+
+### Installation
+
+To install the application follow the steps below in the console, you need to
+have installed nodejs, git and npm.
+
+Download repo:
+
+```
+git clone https://github.com/eddyriv3ra/todo-app-bunny.git
+```
+
+Change to directory:
+
+```
+cd todo-app-bunny
+```
+
+Install backend and frontend dependencies:
+
+```
+npm install
+cd client
+npm install
+```
+
+Run App:
+
+```
+In the repo root run
+npm start
+```
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,10 +8,16 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Runinng'));
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/tasks', require('./routes/api/tasks'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5001;
 
